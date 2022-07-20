c79f1d7b314d51cbd6d6897241baf0f5fb3d60a6
tgmpje <tgm@fluim.net>
2022-07-20 21:55:39 +0200

    WIP: event/webhook support

---
+++ events.go
@@ -0,0 +1,32 @@
+package asana
+
+import (
+	"time"
+)
+
+type EventData struct {
+	Events []Event `json:"events"`
+}
+
+type Event struct {
+	User struct {
+		ID           string `json:"gid"`
+		ResourceType string `json:"resource_type"`
+	} `json:"user"`
+	CreatedAt time.Time   `json:"created_at"`
+	Action    string      `json:"action"`
+	Parent    interface{} `json:"parent"`
+	Change    struct {
+		Field    string `json:"field"`
+		Action   string `json:"action"`
+		NewValue struct {
+			ID           string `json:"gid"`
+			ResourceType string `json:"resource_type"`
+		} `json:"new_value"`
+	} `json:"change"`
+	Resource struct {
+		ID              string `json:"gid"`
+		ResourceType    string `json:"resource_type"`
+		ResourceSubtype string `json:"resource_subtype"`
+	} `json:"resource"`
+}
---
+++ webhooks.go
@@ -0,0 +1,106 @@
+package asana
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Webhook struct {
+	ID           string `json:"gid"`
+	ResourceType string `json:"resource_type"`
+	Active       bool   `json:"active"`
+	Resource     struct {
+		ID           string `json:"gid"`
+		ResourceType string `json:"resource_type"`
+		Name         string `json:"name"`
+	} `json:"resource"`
+	Target    string    `json:"target"`
+	CreatedAt time.Time `json:"created_at"`
+	Filters   []struct {
+		Action          string   `json:"action"`
+		Fields          []string `json:"fields"`
+		ResourceSubtype string   `json:"resource_subtype"`
+		ResourceType    string   `json:"resource_type"`
+	} `json:"filters"`
+	LastFailureAt      time.Time `json:"last_failure_at"`
+	LastFailureContent string    `json:"last_failure_content"`
+	LastSuccessAt      time.Time `json:"last_success_at"`
+}
+type Filters []struct {
+	Action          string   `json:"action"`
+	Fields          []string `json:"fields"`
+	ResourceSubtype string   `json:"resource_subtype"`
+	ResourceType    string   `json:"resource_type"`
+}
+
+// Webhooks returns the compact records for all webhooks your app has registered for the authenticated user in the given workspace.
+func (w *Workspace) Webhooks(client *Client, options ...*Options) ([]*Webhook, *NextPage, error) {
+	client.trace("Listing webhooks for workspace %s...\n", w.ID)
+	var result []*Webhook
+
+	workspace := &Options{
+		Workspace: w.ID,
+	}
+
+	allOptions := append([]*Options{workspace}, options...)
+
+	// Make the request
+	nextPage, err := client.get("/webhooks", nil, &result, allOptions...)
+	return result, nextPage, err
+}
+
+// AllWebhooks repeatedly pages through all available webhooks for a user
+func (w *Workspace) AllWebhooks(client *Client, options ...*Options) ([]*Webhook, error) {
+	var allWebhooks []*Webhook
+	nextPage := &NextPage{}
+
+	var webhooks []*Webhook
+	var err error
+
+	for nextPage != nil {
+		page := &Options{
+			Limit:  100,
+			Offset: nextPage.Offset,
+		}
+
+		allOptions := append([]*Options{page}, options...)
+		webhooks, nextPage, err = w.Webhooks(client, allOptions...)
+		if err != nil {
+			return nil, err
+		}
+
+		allWebhooks = append(allWebhooks, webhooks...)
+	}
+	return allWebhooks, nil
+}
+
+// CreateWebhook registers a new webhook
+func (c *Client) CreateWebhook(resource, target string, filters Filters) (*Webhook, error) {
+	m := map[string]interface{}{}
+	m["resource"] = resource
+	m["target"] = target
+	m["filters"] = filters
+
+	result := &Webhook{}
+
+	err := c.post("/webhooks", m, result)
+
+	return result, err
+}
+
+// DeleteWebhook deletes an existing webhook
+func (c *Client) DeleteWebhook(ID string) error {
+	err := c.delete(fmt.Sprintf("/webhooks/%s", ID))
+	return err
+}
+
+func ParseHook(payload []byte) ([]Event, error) {
+	ed := EventData{}
+	if err := json.Unmarshal(payload, &ed); err != nil {
+		return nil, errors.New("Cannot decode payload")
+	}
+
+	return ed.Events, nil
+}
